8faaa0504c7a75e9318a06eed16064cd3e0dbbf0
muhammadwasi81 <wasiarain819@gmail.com>
2023-04-25 19:57:00 +0500

    update: server.js file

--- backend/server.js
+++ backend/server.js
@@ -21,18 +21,21 @@ app.use("/api/upload", require("./routes/uploadRoutes"));
 app.use(express.static("public"));
 
 // Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("Please set to production");
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "/frontend/build/index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("Please set to production");
+//   });
+// }
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "/frontend/build/index.html"))
+);
 
 app.use(errorHandler);
 
