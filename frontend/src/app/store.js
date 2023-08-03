91869cabfb1465a1e5d20ae712676aae6f409913
muhammadwasi81 <wasiarain819@gmail.com>
2023-08-04 04:17:02 +0500

    update: fixes

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../features/auth/authSlice';
 import cardReducer from '../features/cards/cardSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
     cards: cardReducer,
   },
 });
